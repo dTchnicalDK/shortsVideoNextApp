ddc1058c286b78cfc9b078115d869b9917a49a62
dtechnicaldk <dtechnicaldk@gmail.com>
2026-02-13 14:50:07 +0530

    clerk auth working without db integration

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,9 +4,28 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import React from "react";
 import ModeToggle from "./ModeToggle";
+import {
+  Popover,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
-const NavBar = () => {
+const NavBar = async () => {
   const menus = ["Home", "Profile", "Dashboard"];
+  const signInStatus = false;
+  const session = await auth();
+  console.log("session", session);
   return (
     <div className="min-w-screen border-b-2 shadow-lg flex justify-between items-center">
       <div className="left ">
@@ -21,25 +40,63 @@ const NavBar = () => {
       </div>
 
       <div className="right ">
-        <div className="flex justify-between  gap-5 px-3 ">
+        <div className="flex justify-between  items-center gap-5 px-3 ">
           <ul className="flex justify-center items-center gap-2 text-xl font-semibold">
             {menus.map((menu, key) => (
-              <li key={key}>{menu}</li>
+              <li
+                key={key}
+                className="cursor-pointer hover:text-slate-600 hover:underline  transition-all ease-in-out duration-300"
+              >
+                {menu}
+              </li>
             ))}
           </ul>
+
           <ModeToggle />
-          <div className="space-x-1">
-            <Button>SignIn</Button>
-            <Button>SignOut</Button>
+
+          <div className="space-x-2 flex justify-between items-center my-3">
+            {/* Show the sign-in and sign-up buttons when the user is signed out */}
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <Button>Sign Up</Button>
+              </SignUpButton>
+            </SignedOut>
+            {/* Show the user button when the user is signed in */}
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
         </div>
       </div>
     </div>
   );
 };
 
+const PopoverMenu = () => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Avatar className="cursor-pointer">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverHeader>
+          <PopoverTitle>UserName here</PopoverTitle>
+          <ul className="ml-3 text-lg font-semibold">
+            <li>Profile settings</li>
+            <li>Change password</li>
+            <li>Profile settings</li>
+          </ul>
+        </PopoverHeader>
+        <Separator className="my-3" />
+        <div className="space-x-1 my-3">
+          <Button>SignOut</Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
 export default NavBar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import { ThemeProvider } from "./components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,22 +26,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <div className="min-h-screen">
-            <NavBar />
-            {children}
-          </div>
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="min-h-screen">
+              <NavBar />
+              {children}
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
