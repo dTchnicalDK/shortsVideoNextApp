6d190a64d5a1d0392473b5ce06bbfc7708407964
dtechnicaldk <dtechnicaldk@gmail.com>
2026-02-16 20:54:55 +0530

    logged in user is being saved in neon db successfully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { Plus } from "lucide-react";
 import Link from "next/link";
@@ -6,6 +7,21 @@ import Link from "next/link";
 export default async function Home() {
   const user = await currentUser();
   // console.log("=====", user);
+  if (!user) {
+    return null;
+  }
+  const loggedInUser = await prisma.user.findUnique({
+    where: { clerkUserId: user.id },
+  });
+  if (!loggedInUser) {
+    await prisma.user.create({
+      data: {
+        name: user.fullName || "DummyName",
+        email: user.emailAddresses[0].emailAddress,
+        clerkUserId: user.id,
+      },
+    });
+  }
   if (user) {
     const { firstName, lastName, emailAddresses } = user;
     return (
